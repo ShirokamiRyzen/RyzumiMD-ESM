3c7ffd3d552a9468c74ae725c71766e03657ed12
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-09-01 17:28:38 +0700

    modified:   plugins/downloader-gdrive.js

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -1,24 +1,30 @@
-import { GDriveDl } from '../lib/scrape.js'
+import axios from 'axios'
 
 let handler = async (m, { conn, args }) => {
 	if (!(args[0] || '').match(/([\w-]){33}|([\w-]){19}/)) throw '[!] Input GoogleDrive URL'
 
-	const someincludes = ( data, id ) => {
-        let res = data.find(el => id.includes(el) )
-        return res ? true : false;
-    }
-	
+	const someincludes = (data, id) => {
+		let res = data.find(el => id.includes(el))
+		return res ? true : false
+	}
+
 	try {
-		let res = await GDriveDl(args[0])
-		if (res.fileSize.slice(-2) == "GB") return m.reply(`Ngotak dong.\nMana bisa ngirim video ${res.fileSize}`)
-		if (!someincludes(['kB','KB'], res.fileSize.slice(-2)) && parseInt(res.fileSize) > 500) return m.reply(`Filesize: ${res.fileSize}\nTidak dapat mengirim, maksimal file 500 MB`)
+		const url = `${APIs.ryzumi}/api/downloader/gdrive?url=${encodeURIComponent(args[0])}`
+		const { data: res } = await axios.get(url)
+
+		if (res.fileSize.slice(-2) == 'GB')
+			return m.reply(`Ngotak dong.\nMana bisa ngirim video ${res.fileSize}`)
+		if (!someincludes(['kB', 'KB'], res.fileSize.slice(-2)) && parseInt(res.fileSize) > 500)
+			return m.reply(`Filesize: ${res.fileSize}\nTidak dapat mengirim, maksimal file 500 MB`)
+
 		let txt = `*[ Downloading file ]*\n\n`
 		txt += `*Name :* ${res.fileName}\n`
 		txt += `*Size :* ${res.fileSize}\n`
 		txt += `*Type :* ${res.mimetype}`
 		await m.reply(txt)
-		if (!res.downloadUrl) throw eror
-		await conn.sendFile(m.chat, res.downloadUrl, res.fileName + res.mimetype, res.fileName + res.mimetype, m)
+
+		if (!res.downloadUrl) throw 'Download URL tidak tersedia'
+		await conn.sendFile(m.chat, res.downloadUrl, res.fileName, res.fileName, m)
 	} catch (e) {
 		console.log(e)
 		throw 'Bot tidak memiliki akses ke GoogleDrive ini'
