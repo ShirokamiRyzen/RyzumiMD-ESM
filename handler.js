44a8f203f78c1ec675043f428737d5204c987a4d
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-09-03 17:59:31 +0700

    modified:   handler.js

--- handler.js
+++ handler.js
@@ -128,8 +128,8 @@ export async function handler(chatUpdate) {
         let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
         const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
         const participants = (m.isGroup ? groupMetadata.participants : []) || []
-        const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {} // User Data
-        const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == this.user.jid) : {}) || {} // Your Data
+        const user = (m.isGroup ? participants.find(u => conn.getJid(u.id, m.chat) === m.sender) : {}) || {} // User Data
+        const bot = (m.isGroup ? participants.find(u => conn.getJid(u.id, m.chat) == this.user.jid) : {}) || {} // Your Data
         const isRAdmin = user?.admin == 'superadmin' || false
         const isAdmin = isRAdmin || user?.admin == 'admin' || false // Is User Admin?
         const isBotAdmin = bot?.admin || false // Are you Admin?
