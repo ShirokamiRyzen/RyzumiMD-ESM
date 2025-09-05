a417c485ea56b26edbb788f2bd9f73517c3c2449
AgusXzz <davidmersey707@gmail.com>
2025-09-05 22:33:11 +0000

    Update

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -5,7 +5,7 @@ var handler = async (m, { conn, participants, isAdmin }) => {
     
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : text ? (text.replace(/\D/g, '') + '@s.whatsapp.net') : ''
 		if (!who || who == m.sender) throw 'Reply / tag yang ingin di kick'
-		if (participants.filter(v => v.id == who).length == 0) throw `Target tidak berada dalam Grup !`
+		if (participants.filter(v => v.jid == who).length == 0) throw `Target tidak berada dalam Grup !`
 		await conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
     m.reply(`Success`)
 }
