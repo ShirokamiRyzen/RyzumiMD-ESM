1358d670ec2f0e3cf6a507458770cd1bc4307d88
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-09-26 21:55:21 +0700

    WIP: local fix (temp)

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ loadDatabase()
 const { state, saveCreds } = await useMultiFileAuthState('./sessions')
 const connectionOptions = {
   logger: pino({ level: 'fatal' }),
-  browser: ['Mac OS', 'safari', '5.1.10'],
+  browser: ["Ubuntu", "Chrome", "20.0.04"],
   auth: {
     creds: state.creds,
     keys: makeCacheableSignalKeyStore(state.keys, pino().child({
